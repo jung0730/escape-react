b84824522c55da7166218f933652dcdf50f28316
Rebecca <rebecca.yu@senao.com.tw>
2022-10-11 17:34:44 +0800

    fix ArticleDetail jsx

--- src/pages/ArticleDetail.jsx
+++ src/pages/ArticleDetail.jsx
@@ -33,20 +33,20 @@ function ArticleDetail () {
               <form>
                 <div className="mb-3">
                   <label htmlFor="title" className="form-label">文章名稱</label>
-                  <input type="text" className="form-control" id="title" value={article.title} onChange={handleChange} />
-               </div>
+                  <input type="text" className="form-control" value={article.title} id="title" onChange={handleChange} />
+                </div>
                 <div className="mb-3">
                   <label htmlFor="author" className="form-label">文章作者</label>
-                  <input type="text" className="form-control" id="author" value={article.author} onChange={handleChange} />
+                  <input type="text" className="form-control" value={article.author} id="author" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                   <label htmlFor="content" className="form-label">文章描述</label>
-                  <textarea className="form-control" id="description" value={article.description} onChange={handleChange} />
+                  <textarea className="form-control" id="content" value={article.content} onChange={handleChange} />
                 </div>
                 <div className="row gx-1 mb-3">
                   <div className="col-md-2 mb-1">
                     <div className="input-group input-group-sm">
-                      <input type="text" className="form-control form-control" id="tag" placeholder="請輸入標籤" value={article.tag} onChange={handleChange} />
+                      <input type="text" className="form-control" value={article.tag} id="tag" placeholder="請輸入標籤" onChange={handleChange} />
                       <button type="button" className="btn btn-outline-danger">x</button>
                     </div>
                   </div>
@@ -61,9 +61,9 @@ function ArticleDetail () {
               <h5 className="card-title">文章圖片</h5>
             </div>
             <div className="card-body">
-            <img src={article.image} className="image-fluid rounded mb-3" alt={article.title} />
-            <label className="d-block" htmlFor="image">文章圖片連結</label>
-            <input type="text" className="form-control" value={article.image} id="image" onChange={handleChange} />
+              <img src={article.image} className="image-fluid rounded mb-3" alt={article.title} />
+              <label className="d-block" htmlFor="image">文章圖片連結</label>
+              <input type="text" className="form-control" value={article.image} id="image" onChange={handleChange} />
             </div>
           </div>
         </div>
